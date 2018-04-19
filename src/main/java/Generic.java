454bdd84c3cfc02e966a20a8995393d3ded20f63
lexa <sokolov@ozero.net>
2018-04-19 23:03:21 +0300

    Locking how the methods works

--- src/main/java/Generic.java
+++ src/main/java/Generic.java
@@ -1,3 +1,12 @@
 public class Generic {
 
+    public static <E> E printE(E element) {
+        return element;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(Generic.printE(1123123));
+
+    }
 }
