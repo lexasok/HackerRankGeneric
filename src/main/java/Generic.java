387f87816327e5f6f619713d4c375530dff90a67
lexa <sokolov@ozero.net>
2018-04-20 01:30:40 +0300

    Solution done

--- src/main/java/Generic.java
+++ src/main/java/Generic.java
@@ -1,6 +1,40 @@
+import java.util.Scanner;
+
 public class Generic {
 
     public static void main(String[] args) {
 
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        Integer[] intArray = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            intArray[i] = scan.nextInt();
+        }
+
+        n = scan.nextInt();
+        String[] stringArray = new String[n];
+        for (int i = 0; i < n; i++) {
+            stringArray[i] = scan.next();
+        }
+
+        Printer<Integer> intPrinter = new Printer<Integer>();
+        Printer<String> stringPrinter = new Printer<String>();
+        intPrinter.printArray( intArray  );
+        stringPrinter.printArray( stringArray );
+        if(Printer.class.getDeclaredMethods().length > 1){
+            System.out.println("The Printer class should only have 1 method named printArray.");
+        }
+
+    }
+
+}
+
+class Printer<T> {
+
+    public <T> void printArray(T[] intArray) {
+        for (T t : intArray) {
+            System.out.println(t);
+        }
     }
 }
