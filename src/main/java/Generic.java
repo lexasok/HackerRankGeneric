3513710628bfbb32ac62dbc0829b59ce8c42dc88
lexa <sokolov@ozero.net>
2018-04-20 01:18:12 +0300

    Old code deleted

--- src/main/java/Generic.java
+++ src/main/java/Generic.java
@@ -1,15 +1,6 @@
 public class Generic {
 
-    public static <T extends Comparable<T>> T findMax(T a, T b) {
-        int n = a.compareTo(b);
-        if (n < 0) {
-            return b;
-        } else return a;
-    }
-
     public static void main(String[] args) {
-
-        System.out.println(findMax("asdads","as1111111112"));
 
     }
 }
